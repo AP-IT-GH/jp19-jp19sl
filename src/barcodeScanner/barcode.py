ecc6d0c06d9084ac869c47fd2768ddc148627123
jarnomehj <coolsjarno@gmail.com>
2019-03-13 15:39:20 +0100

    Added: Able to filter student number from barcode

--- src/barcodeScanner/barcode.py
+++ src/barcodeScanner/barcode.py
@@ -5,5 +5,7 @@ prefix = ' '
 while prefix != ']C1S0000':
     barcode = input("Scan uw studentenkaart")
     prefix = barcode[0:8]
-
+    if prefix != ']C1S0000':
+        print("Geen studentenkaart van AP")
+print (barcode[8:14])
 print("Prefix is juist studentenkaart van AP")
