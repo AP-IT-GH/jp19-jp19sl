fe6d8ad0a9723e960fa90744150b175c2a604c52
jarnomehj <coolsjarno@gmail.com>
2019-03-17 14:06:34 +0100

    aaa

--- src/barcodeScanner/barcode.py
+++ src/barcodeScanner/barcode.py
@@ -1,11 +1,24 @@
-import re
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
 
 prefix = ' '
 
 while prefix != ']C1S0000':
     barcode = input("Scan uw studentenkaart")
     prefix = barcode[0:8]
+    
     if prefix != ']C1S0000':
         print("Geen studentenkaart van AP")
-print (barcode[8:14])
+
+snummer = barcode[8:14]
+print(snummer)
 print("Prefix is juist studentenkaart van AP")
+
+GPIO.output(17, True)
+time.sleep(5)
+GPIO.output(17, False)
+
+GPIO.cleanup()
