c9383fdf39888eb5dfc11c0d790ebeda35bfb0a0
oussama Es-Salhi <oussie11@gmail.com>
2019-04-01 18:55:54 +0200

    added socket.io and mongodb change streams

--- src/nodejsApiServer/dist/server.js
+++ src/nodejsApiServer/dist/server.js
@@ -12,39 +12,47 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const bodyParser = __importStar(require("body-parser"));
 const express_1 = __importDefault(require("express"));
-// import * as mongoose from "mongoose";
 const mongoose = require("mongoose");
 const locker_1 = require("./models/locker");
 const reservation_1 = require("./models/reservation");
 const student_1 = require("./models/student");
 const app = express_1.default();
-mongoose.connect("mongodb://localhost:27017/smartlocker").catch((err) => console.log(err));
-// this will let us get the data from a POST
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
+const changeStream = locker_1.Locker.watch(); // { fullDocument: "updateLookup" } meegeven als parameter voor het volledige document terug te krijgen
+mongoose.connect("mongodb://localhost:27017/smartlocker?replicaSet=rs0").catch((err) => console.log(err));
+changeStream.on("change", (change) => {
+    console.log(change); // You could parse out the needed info and send only that data.
+    io.emit("changeData", change);
+});
+io.on("connection", function (client) {
+    console.log("Client " + client.id + " connected...");
+    // client.on("join", function(data) {
+    // 	console.log(data);
+    // });
+});
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
-const port = process.env.PORT || 8080;
+const port = 3000;
 const router = express_1.default.Router();
 // // REGISTER OUR ROUTES -------------------------------
 // // all of our routes will be prefixed with /api
 // ------------- LOCKER ---------------------------------------
 // GET localhost:8080/api/lockers
 app.use("/api", router);
-router.route("/lockers")
-    .get((req, res) => {
-    locker_1.Locker.find()
-        .populate("reservation")
-        .then((data) => {
+router.route("/lockers").get((req, res) => {
+    locker_1.Locker.find().populate("reservation").then((data) => {
         res.json(data);
     });
 });
 // GET localhost:8080/api/lockers/<id>
-router.route("/lockers/:id")
+router
+    .route("/lockers/:id")
     .get((req, res) => {
     const id = req.params.id;
-    locker_1.Locker.findById(id)
-        .then((data) => {
+    locker_1.Locker.findById(id).then((data) => {
         res.json(data);
     });
 })
@@ -62,10 +70,10 @@ router.route("/lockers/:id")
     });
 });
 // ---------------- STUDENTS -----------------------------
-router.route("/students")
+router
+    .route("/students")
     .get((req, res) => {
-    student_1.Student.find()
-        .then((data) => {
+    student_1.Student.find().then((data) => {
         res.json(data);
     });
 })
@@ -80,11 +88,11 @@ router.route("/students")
         }
     });
 });
-router.route("/students/:id")
+router
+    .route("/students/:id")
     .get((req, res) => {
     const id = req.params.id;
-    student_1.Student.findById(id)
-        .then((data) => {
+    student_1.Student.findById(id).then((data) => {
         res.json(data);
     });
 })
@@ -102,11 +110,10 @@ router.route("/students/:id")
     });
 });
 // ---------------- Reservations -----------------------------
-router.route("/reservations")
+router
+    .route("/reservations")
     .get((req, res) => {
-    reservation_1.Reservation.find()
-        .populate("student")
-        .then((data) => {
+    reservation_1.Reservation.find().populate("student").then((data) => {
         res.json(data);
     });
 })
@@ -122,11 +129,11 @@ router.route("/reservations")
         }
     });
 });
-router.route("/reservations/:id")
+router
+    .route("/reservations/:id")
     .get((req, res) => {
     const id = req.params.id;
-    reservation_1.Reservation.findById(id)
-        .then((data) => {
+    reservation_1.Reservation.findById(id).then((data) => {
         res.json(data);
     });
 })
@@ -143,6 +150,6 @@ router.route("/reservations/:id")
         }
     });
 });
-app.listen(port);
+server.listen(port);
 console.log("127.0.0.1:" + port);
 //# sourceMappingURL=server.js.map
