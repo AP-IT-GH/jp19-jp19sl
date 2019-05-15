12ee0d6f05598dfb4a019bd57cad78831a5ff954
jarnomehj <coolsjarno@gmail.com>
2019-05-15 16:13:12 +0200

    able to do put request to api via ionic

--- src/SmartLockerWebsite/src/app/deposit-withdraw/deposit-withdraw.page.ts
+++ src/SmartLockerWebsite/src/app/deposit-withdraw/deposit-withdraw.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LockerService } from '../services/locker.service';
 
 @Component({
   selector: 'app-deposit-withdraw',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepositWithdrawPage implements OnInit {
 
-  constructor() { }
+  constructor(private lockerSvc: LockerService) { }
 
   ngOnInit() {
   }
--- src/SmartLockerWebsite/src/app/services/locker.service.ts
+++ src/SmartLockerWebsite/src/app/services/locker.service.ts
@@ -11,15 +11,49 @@ export class LockerService {
 	constructor(private httpSvc: HttpClient, private barcodeSvc: BarcodeReaderService) { }
 
 	GetStudents() {
-		this.httpSvc.get(this.apiUrl + "/students/" + this.barcodeSvc.studentNummer).subscribe(success => {
-			console.log(success);
-		})
+		this.httpSvc.get(this.apiUrl + "/students/" + this.barcodeSvc.studentNummer)
+			.subscribe(success => {
+				console.log(success);
+			});
 	}
+
+	OpenLocker(lockerId) {
+		console.log("Open locker function")
+		this.httpSvc.put(this.apiUrl + "/lockers/" + lockerId, { "open": true })
+			.subscribe(success => {
+
+			});
+	}
+
 }
 
-interface Locker {
-	open: boolean,
-	users: string[],
-	lockerName: string,
-	lokaal: string
+export interface IStudent {
+	_id: string;
+	name: string;
+	lastName: string;
+	student_number: number;
+	group: string;
+	__v: number;
+	created_at: Date;
+}
+
+export interface IReservation {
+	student: IStudent[];
+	_id: string;
+	locker: string;
+	startDate: Date;
+	endDate: Date;
+	created_at: Date;
+	__v: number;
+}
+
+export interface ILocker {
+	reservation: IReservation[];
+	_id: string;
+	open: boolean;
+	className: string;
+	lockerName: string;
+	isReserved: boolean;
+	created_at: Date;
+	__v: number;
 }
